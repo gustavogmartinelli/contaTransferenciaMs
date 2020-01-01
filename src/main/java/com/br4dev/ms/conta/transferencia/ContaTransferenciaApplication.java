cb52196b08311f1cc6ff5753cbecee2175b92c96
Gustavo G. Martinelli <gustavogmartinelli@gmail.com>
2020-01-01 20:52:32 -0300

    primeiro Commit

---
+++ src/main/java/com/br4dev/ms/conta/transferencia/ContaTransferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.br4dev.ms.conta.transferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaTransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaTransferenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br4dev/ms/conta/transferencia/ContaTransferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br4dev.ms.conta.transferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaTransferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
